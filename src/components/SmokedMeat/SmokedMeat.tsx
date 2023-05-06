979ef7a1593ff32227183f4ac78eec4d38fc13cc
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-05-06 23:53:38 +0500

    feat: add test response

--- src/components/SmokedMeat/SmokedMeat.tsx
+++ src/components/SmokedMeat/SmokedMeat.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import c from './SmokedMeat.module.css';
+import axios from 'axios';
+
+export type ProdType = {
+    id: number
+    name_prod: string
+    unit: string
+    price: string
+}
+export type ProdResponseType = ProdType[]
 
 export const SmokedMeat = (props: any) => {
 
-    return <div className={c.smokedMeat}>
+    const [item, setItem] = useState<ProdResponseType>([]);
 
+    useEffect(() => {
+        axios.get('http://bibak007.pythonanywhere.com/copch/')
+            .then(res=> {
+                setItem(res.data)
+            })
+    }, []);
+
+    return <div className={c.smokedMeat}>
+        <table className={c.smokedMeatTable}>
+            <thead>
+            <tr>
+                <th>Позиция</th>
+                <th>Наименование</th>
+                <th>Единица товара</th>
+                <th>Стоимость за ед.</th>
+            </tr>
+            </thead>
+            <tbody>
+            {item.map(el=> {
+                return (
+                    <tr>
+                        <td><span>{el.id}</span></td>
+                        <td><span>{el.name_prod}</span></td>
+                        <td><span>{el.unit}</span></td>
+                        <td><span>{el.price}</span></td>
+                    </tr>
+                )
+            })}
+            </tbody>
+        </table>
     </div>
 }
